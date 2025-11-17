c56c276f57ffa5191e066e14167ffcbbb9db564c
mhmdshiddiq17 <mhmdshiddiq17@gmail.com>
2025-11-17 15:07:15 +0700

    zsszsz

---
+++ server/inngest/index.js
@@ -0,0 +1,53 @@
+import {Inngest} from 'inngest';
+import prisma from '../configs/prisma';
+
+export const inngest = new Inngest({ id: "pm-beargoat" });
+
+const syncUserCreation = inngest.createFunction(
+  { id: "sync-user-from-clerk" },
+  { event: "clerk/user.created" },
+  async ({ event }) => {
+    const { data } = event;
+    await prisma.user.create({
+      id: data.id,
+      email: data.email_addresses[0]?.email_address || '',
+      name: data.first_name + " " + data.last_name,
+      image: data.profile_image_url
+    })
+  }
+)
+const syncUserDeletion = inngest.createFunction(
+  { id: "delete-user-with-clerk" },
+  { event: "clerk/user.deleted" },
+  async ({ event }) => {
+    const { data } = event;
+    await prisma.user.delete({
+      where: {
+        id: data.id,
+      }
+    })
+  }
+)
+const syncUserUpdation = inngest.createFunction(
+  { id: "update-user-from-clerk" },
+  { event: "clerk/user.updated" },
+  async ({ event }) => {
+    const { data } = event;
+    await prisma.user.update({
+      where: {
+        id: data.id,
+      },
+      data: {
+          email: data.email_addresses[0]?.email_address || '',
+          name: data.first_name + " " + data.last_name,
+          image: data.profile_image_url
+      }
+    })
+  }
+)
+
+
+
+
+
+export const functions = [syncUserCreation, syncUserDeletion, syncUserUpdation];
---
+++ server/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import {ClerkMiddleware} from '@clerk/express'
+import { functions, inngest } from './inngest';
+import { serve } from 'inngest/express';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(ClerkMiddleware());
+
+app.get('/', (req, res) => { 
+  res.send('server is live')
+})
+
+app.use("/api/inngest", serve({client: inngest, functions}))
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
